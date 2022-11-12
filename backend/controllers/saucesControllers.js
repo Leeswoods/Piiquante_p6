6fa350d8e30572e3d1732b273f5dcfd5fbdb6edd
Damien Ouvrard <ouvrard.damien33@gmail.com>
2022-11-12 13:26:38 +0100

    Pseudo_code_controllers_sauces

---
+++ backend/controllers/saucesControllers.js
@@ -0,0 +1,21 @@
+// Importer le Sauce Models
+
+
+// Créaction d'une sauce 
+
+
+// Obtenir une sauce grâce à l'ID 
+
+
+
+// Obtenir toutes les sauces
+
+
+
+// Modifier une sauce 
+
+
+// Supprimer une sauce 
+
+
+// Likes ou Dislikes une sauce 
--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -3,6 +3,7 @@
 const multer = require('multer');
 
 // Dictionnaire (est un objet)
+// Générer l'extension du fichier
 const MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
@@ -10,17 +11,26 @@ const MIME_TYPES = {
 }
 
 // Objet de configuration pour Multer 
-// Multer fonction diskStorage 
+
+//Nous créons une constante storage , à passer à multer comme configuration, qui contient la logique nécessaire pour indiquer à multer où enregistrer les fichiers entrants :
+// Multer fonction diskStorage => configure le chemin et le nom de fichier pour les fichiers entrants.
+
 const storage = multer.diskStorage({
+    // la fonction destination indique à multer d'enregistrer les fichiers dans le dossier images ;
     destination: (req, file, callback) => {
         callback(null, 'images')
     },
+    // Explique à Multer quel nom de fichier autorisé 
+    // la fonction filename indique à multer d'utiliser le nom d'origine, de remplacer les espaces par des underscores et d'ajouter un timestamp Date.now() comme nom de fichier. 
+    // Elle utilise ensuite la constante dictionnaire de type MIME pour résoudre l'extension de fichier appropriée.
     filename: (req, files, callback) => {
         const name = file.originalname.split('').join('_');
+        // Générer l'extension du fichier
         const extension = MIME_TYPES[file.mimetype];
         callback(null, name + Date.now() + '.' + extension);
     }
 });
 
 // Exporter notre module multer
-module.exports = multer({  storage }).single('image');
+// Méthode single()  crée un middleware qui capture les fichiers d'un certain type (passé en argument), et les enregistre au système de fichiers du serveur à l'aide du storage configuré.
+module.exports = multer({storage: storage}).single('image');
